d09fe4a4d5dbff902633762235adadb1089b0aa3
Abhi Kamarthi <kamarthiabhishek@gmail.com>
2026-07-16 13:15:43 +0530

    Project setup

---
+++ src/test/java/com/devprep/DevPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
